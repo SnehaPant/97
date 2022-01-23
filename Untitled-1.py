e21741fac5a9cc010928cb3c6f150e4d9a174b0c
SnehaPant <72789523+SnehaPant@users.noreply.github.com>
2022-01-23 19:41:55 +0530

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,48 @@
+game="number guessing game"
+print(game)
+number=int(input("enter your guess:"))
+
+if(number<=3):
+    print("your guess was too low")
+elif(number<=8):
+    print("your guess was too low")
+else:
+    print("congratulation you won")
+
+
+number=int(input("enter your guess:"))
+
+if(number<=3):
+    print("your guess was too low")
+elif(number<=8):
+    print("your guess was too low")
+else:
+    print("congratulation you won")
+number=int(input("enter your guess:"))
+
+if(number<=3):
+    print("your guess was too low")
+elif(number<=8):
+    print("your guess was too low")
+else:
+    print("congratulation you won")
+number=int(input("enter your guess:"))
+
+if(number<=3):
+    print("your guess was too low")
+elif(number<=8):
+    print("your guess was too low")
+else:
+    print("congratulation you won")
+number=int(input("enter your guess:"))
+
+if(number<=3):
+    print("your guess was too low")
+elif(number<=8):
+    print("your guess was too low")
+else:
+    print("congratulation you won")
+count=5
+while(count>=0):
+    print("you loose")
+    count=count-6
